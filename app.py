e09c2eaf0c503ca0c3953a2a4e4bbb1b0456b162
test <test@test>
2022-11-07 20:59:36 +0100

    update

--- app.py
+++ app.py
@@ -3,13 +3,9 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
+
 @app.route('/')
 def home():
     return render_template("index.html")
 
 
-if __name__ == "__app__":
-   app.run(debug=False)
-
-
-home()
